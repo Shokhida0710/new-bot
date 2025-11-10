21976885a079ed107b76aee8b8b529d412438781
shokhida <shohidaomonova008@gmail.com>
2025-11-10 17:35:50 +0500

    ...sh

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import TelegramBot from "node-telegram-bot-api";
 const TOKEN = "8238239595:AAED9NKuyLn97sNvaNpjzAqDxxJWSFy4sSw"
 const bot = new TelegramBot(TOKEN, { polling: true });
-bot.on("message", (msg) => {
+bot.on("message", function (msg) {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, "Welcome " + msg.text);
+} );
