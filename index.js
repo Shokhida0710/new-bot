0b7fa4080f5e6f1ba5841c6bd842173bc06fc891
shokhida <shohidaomonova008@gmail.com>
2025-11-10 17:44:34 +0500

    ...sho

--- index.js
+++ index.js
@@ -3,5 +3,18 @@ const TOKEN = "8238239595:AAED9NKuyLn97sNvaNpjzAqDxxJWSFy4sSw"
 const bot = new TelegramBot(TOKEN, { polling: true });
 bot.on("message", function (msg) {
     const chatId = msg.chat.id;
-    bot.sendMessage(chatId, "Welcome " + msg.text);
+    const text = msg.text;
+    const firstName = msg.from.first_name;
+    if (text === "/start") {
+    bot.sendMessage(chatId, "Welcome  , ${ firstName}" , {
+        reply_markup: {
+            keyboard: [
+                [{ text: "Send me a photo" }],
+                [{ text: "Send me a sticker" }]
+            ],
+             resize_keyboard: true,
+
+    }
+    });
+
 } );
